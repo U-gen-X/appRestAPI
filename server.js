672be1f88d3bc5d4db1375e2812c0990c2a38474
U-gen-X <cooch.ugenx@gmail.com>
2023-07-18 09:46:09 +0700

    setting routes di server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const { listen } = require('express/lib/application');
+const bodyParser = require("body-parser");
+const express = require("express");
+const { listen } = require("express/lib/application");
 const app = express();
 
 // parse aplikasi/json
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const routes = require("./routers");
+routes(app);
 
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
-
-
-
