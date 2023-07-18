543dfcedcf015dad4712cb6b0f0e16fba4f441c0
U-gen-X <cooch.ugenx@gmail.com>
2023-07-18 10:14:38 +0700

    menambahkan modul tampilsemuadata di controller dan routers

--- controller.js
+++ controller.js
@@ -4,5 +4,15 @@ const response = require("./res");
 const connection = require("./koneksi");
 
 exports.index = function (req, res) {
-    response.ok("Aplikasi Rest API berjalan");
+    response.ok("Aplikasi Rest API berjalan", res);
+};
+
+exports.tampilsemuadata = function (req, res) {
+    connection.query("SELECT * FROM mahasiswa", function (error, rows, fields) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
 };
