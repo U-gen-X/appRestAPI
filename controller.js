b3ac6dbb79577bfdcd6493b12656453af19c664a
U-gen-X <cooch.ugenx@gmail.com>
2023-07-18 10:36:39 +0700

    menambahkan modul tampil data berdasarkan id di controller dan routes

--- controller.js
+++ controller.js
@@ -7,12 +7,29 @@ exports.index = function (req, res) {
     response.ok("Aplikasi Rest API berjalan", res);
 };
 
+// menampilkan semua data mahasiswa
 exports.tampilsemuadata = function (req, res) {
     connection.query("SELECT * FROM mahasiswa", function (error, rows, fields) {
         if (error) {
-            connection.log(error);
+            console.log(error);
         } else {
             response.ok(rows, res);
         }
     });
+};
+
+// menampilkan semua data berdasarkan id
+exports.tampildataid = function (req, res) {
+    let id = req.params.id;
+    connection.query(
+        "SELECT * FROM mahasiswa WHERE id_mhs = ?",
+        [id],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok(rows, res);
+            }
+        }
+    );
 };
--- routers.js
+++ routers.js
@@ -6,4 +6,6 @@ module.exports = function (app) {
     app.route("/").get(jsonku.index);
 
     app.route("/tampil").get(jsonku.tampilsemuadata);
+
+    app.route("/tampil/:id").get(jsonku.tampildataid);
 };
